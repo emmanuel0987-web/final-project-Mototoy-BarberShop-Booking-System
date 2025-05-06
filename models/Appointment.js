ce1d988fe2139b0ded03c0b97fc0ec7cf3d6ed80
bitoytoy-gif <rinonjamesnoel@gmail.com>
2025-05-06 19:20:15 +0800

    Update Appointment.js

--- models/Appointment.js
+++ models/Appointment.js
@@ -1,12 +1,13 @@
 import mongoose from 'mongoose';
 
-const AppointmentSchema = new mongoose.Schema({
+const appointmentSchema = new mongoose.Schema({
   name: { type: String, required: true },
   service: { type: String, required: true },
   date: { type: String, required: true },
-  timeSlot: { type: String, required: true },
+  timeSlot: { type: String, required: true }
 }, { timestamps: true });
 
-AppointmentSchema.index({ date: 1, timeSlot: 1 }, { unique: true });
+// Prevent double-booking same date/time
+appointmentSchema.index({ date: 1, timeSlot: 1 }, { unique: true });
 
-export default mongoose.model('Appointment', AppointmentSchema);
+export default mongoose.model('Appointment', appointmentSchema);
